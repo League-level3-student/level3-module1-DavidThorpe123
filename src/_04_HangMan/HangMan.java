5422cbafd398477d39b0443866e0f93cafbe1a9a
Rohan890 <league@WTS7.attlocal.net>
2018-09-29 17:25:08 -0700

    almost done
    donee

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,41 @@
 package _04_HangMan;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 public class HangMan{
+	JFrame frame;
+	JPanel panel;
+	JLabel label;
+	HangMan() {
+		frame = new JFrame();
+		panel = new JPanel();
+		label = new JLabel();
+		frame.add(panel);
+		panel.add(label);
+
+	}
+public static void main(String[] args) {
+	String numberString = JOptionPane.showInputDialog("Enter a number");
+	int numberInt = Integer.parseInt(numberString);
+
+		for (int i = 0; i < numberInt; i++) {
+			Stack<String> stack = new Stack();
+			stack.push(Utilities.readRandomLineFromFile("src/dictionary.txt"));
+			
+		}
+		
+		
 	
+}	
 	
 	
 }
